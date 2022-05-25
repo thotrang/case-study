947451cf051afeffffdd360eae957f323d08a6ae
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-05-25 22:47:55 +0700

    secon commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-let arrProduct = [];
+let product1 = new Product('01', '..\img\ruou-bia\bia.png', '15000', 'Bia', '50');
+let product2 = new Product('02', '..\img\ruou-bia\ruou.png', '50000', 'Rượu', '50');
+let arrProduct = [product1, product2]
 showTable();
 // add product
 function addProduct() {
@@ -22,7 +24,7 @@ function showTable() {
         tableP += `<tr>
                 <td>${arrProduct[i].id}</td>
                 <td>${arrProduct[i].name}</td>
-                <td><a href="${arrProduct[i].image}"></a></td>
+                <td><a href="${arrProduct[i].image}"></a></td>zz
                 <td>${arrProduct[i].price}</td>
                 <td>${arrProduct[i].amount}</td>
                 <td><button id="edit" onclick="showEdit(${i})">Edit Product</button></td>
@@ -31,37 +33,30 @@ function showTable() {
     }
     document.getElementById("tableProduct").innerHTML = tableP;
 }
-// edit product
-function showEdit(index){//tạo ra hàng edit
-    let text = `<td><input type="number" id="editId"></td>
-                <td><input type="text" id="editName"></td>
-                <td><input type="text" id="editImage"></td>
-                <td><input type="text" id="editPrice"></td>
-                <td><input type="text" id="editAmount"></td>
-                <td><button onclick="editProduct(${index})">Edit</button></td>
-                <td></td>`
-                console.log(text)
-     document.getElementById("showEditProduct").innerHTML=text;            
+//edit product
+function showEdit(index) {//tạo ra thanh edit
+    let text = `<button onclick="editProduct(${index})">Edit</button>`
+    document.getElementById("showEditProduct").innerHTML = text;
     //xuất hiện giá trị cũ trong ô input
-    document.getElementById("editId").value=arrProduct[index].id;
-    document.getElementById("editName").value=arrProduct[index].name;
-    document.getElementById("editPrice").value=arrProduct[index].price;
-    document.getElementById("editAmount").value=arrProduct[index].amount;
-    document.getElementById("editImage").value=arrProduct[index].image;
+    document.getElementById("editId").value = arrProduct[index].id;
+    document.getElementById("editName").value = arrProduct[index].name;
+    document.getElementById("editPrice").value = arrProduct[index].price;
+    document.getElementById("editAmount").value = arrProduct[index].amount;
+    document.getElementById("editImage").value = arrProduct[index].image;
 
-    document.getElementById("showEditProduct").style.display = 'block';//hiện lên thanh edit
+
+    document.getElementById("tableEdit").style.display = 'block';//hiện lên thanh edit
     //
 
 }
-function editProduct(index){
-    let a = document.querySelectorAll(".ooo");
-    arrProduct[index].id=document.getElementById("editId").value
-    arrProduct[index].name=document.getElementById("editName").value
-    arrProduct[index].price=document.getElementById("editPrice").value
-    arrProduct[index].amount=document.getElementById("editAmount").value
-    arrProduct[index].image=document.getElementById("editImage").value
+function editProduct(index) {
+    arrProduct[index].id = document.getElementById("editId").value
+    arrProduct[index].name = document.getElementById("editName").value
+    arrProduct[index].price = document.getElementById("editPrice").value
+    arrProduct[index].amount = document.getElementById("editAmount").value
+    arrProduct[index].image = document.getElementById("editImage").value
     showTable();
-    document.getElementById("showEditProduct").style.display = 'none';
+    document.getElementById("tableEdit").style.display = 'none';
 }
 // xóa product
 function delProduct(index) {
@@ -71,5 +66,12 @@ function delProduct(index) {
     }
     showTable();
 }
-
+// function clear(){
+//     document.getElementById("editId").value='';
+//     document.getElementById("editName").value='';
+//     document.getElementById("editPrice").value='';
+//     document.getElementById("editAmount").value='';
+//     document.getElementById("editImage").value='';
+//     console.log('1')
+// }
 
