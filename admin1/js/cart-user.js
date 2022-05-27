489d39a7b58e40ff689ff82e9715ceee5b93fd38
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-05-27 13:40:10 +0700

    commit 8

---
+++ admin1/js/cart-user.js
@@ -0,0 +1,6 @@
+function showCart(){
+    document.getElementById("carts").style.display='block';
+}
+function noneCart(){
+    document.getElementById("carts").style.display='none';
+}
---
+++ admin1/js/log-in-user.js
@@ -0,0 +1,6 @@
+function showlogIn(){
+    document.getElementById("logIns").style.display='block';
+}
+function noneLogin(){
+    document.getElementById("logIns").style.display='none';
+}
